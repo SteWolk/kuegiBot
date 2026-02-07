91423fe27064fc2ea54260d500a54656e3d4aa29
Stephan Wolkow <stephan.wolkow@gmail.com>
2026-02-07 21:00:41 +0100

    removed bitmex due to future package dependency

--- kuegi_bot/exchanges/bitmex/auth/__init__.py
+++
@@ -1,3 +0,0 @@
-from kuegi_bot.exchanges.bitmex.auth.AccessTokenAuth import *
-from kuegi_bot.exchanges.bitmex.auth.APIKeyAuth import *
-from kuegi_bot.exchanges.bitmex.auth.APIKeyAuthWithExpires import *
